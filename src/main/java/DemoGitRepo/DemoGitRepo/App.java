1b101af0e1f42fcafb95f24b861608fe2eadd647
sapna <sapna994patil@gmail.com>
2024-12-31 17:11:09 +0530

    first git commit for my selenium code

---
+++ src/main/java/DemoGitRepo/DemoGitRepo/App.java
@@ -0,0 +1,10 @@
+package DemoGitRepo.DemoGitRepo;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
